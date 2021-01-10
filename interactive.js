8317c19aa3a16fc99ac7e21c82b8e3fc3964c9c5
Saad Mukhtar <saadmukhtar01@gmail.com>
2021-01-10 16:17:36 -0500

    Initial Commit

---
+++ interactive.js
@@ -0,0 +1,12 @@
+$("div").click(function () {
+
+    if ($(this).attr("id") == "circle") {
+
+      alert("You clicked on a circle");
+
+    } else {
+
+      alert("You clicked on a square")
+    }
+
+  });
